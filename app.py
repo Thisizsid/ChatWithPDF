725896212b8b0ef497f9db8012d4b4a6423d8bf9
Thisizsid <103445129+Thisizsid@users.noreply.github.com>
2025-04-01 14:04:52 +0545

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+os.environ['HF_TOKEN']="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"
 embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 # embeddings = OpenAIEmbeddings()
 
